f81cdd2480228853a5414dca393bde88763050ca
Aysylu <aysylu_bad@mail.ru>
2024-03-30 20:24:50 +0500

    Добавьте файлы проекта.

---
+++ Bits.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home2
+{
+    internal class Bits
+    {
+        public Bits(long a)
+        {
+            this.Value = a;
+        }
+        public Bits(int b)
+        {
+            this.Value2 = b;
+        }
+
+        public Bits(byte c)
+        {
+            this.Value3 = c;
+        }
+        public long Value { get; private set; } = 0;
+        public int Value2 { get; private set; } = 0;
+        public byte Value3 { get; private set; } = 0;
+
+
+        public static explicit operator long(Bits a) => (long)a.Value;
+        public static implicit operator Bits(long a) => new (a);
+
+        public static explicit operator int(Bits b) => (int)b.Value2;
+        public static implicit operator Bits(int b) => new (b);
+
+        public static explicit operator byte(Bits c) => (byte)c.Value3;
+        public static implicit operator Bits(byte c) => new (c);
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Реализуйте операторы неявного приведения из long,int,byt в Bits.
+namespace Home2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long a = 20;
+            var bits = (Bits)a;
+            Console.WriteLine(bits.Value);
+
+            int b = 50;
+            var bits1 = (Bits)b;
+            Console.WriteLine(bits1.Value2);
+
+            byte d = 70;
+            var bits2 = (Bits)d;
+            Console.WriteLine(bits2.Value3);
+
+        }
+    }
+
+}
+
